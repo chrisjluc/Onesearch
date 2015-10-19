a81a3d31d6ef508a1d9fd34bbd372d6c80ede365
Stephen Luc <stephenluc96@gmail.com>
2015-10-19 17:47:51 -0400

    Updated and easier to read

--- word_scripts/wordfind_site.py
+++ word_scripts/wordfind_site.py
@@ -1,26 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
 
-def wordFindSite ():
+def extract(htmlList):
+    words = []
+    
+    finding_words = list(filter(lambda x: x.get('class') == ['defLink'], htmlList))
+    
+    for link in finding_words:
+        words.append(link.find('a').get('href')[6:-1])     
+        
+    return words
+
+def extract_from_word_find_site():
     wordFind_dict = {}
     for word_length in range(3,12):
         try:
             req = requests.get("http://www.wordfind.com/{0}-letter-words/".format(word_length))
-        except:
-            pass
-        
-        unextracted = BeautifulSoup(req.text, 'html.parser').find_all('li')
-        
-        def extract (htmlList):
-            words = []
-            
-            finding_words = list(filter(lambda x: True if x.get('class') == ['defLink'] else False, htmlList))
-            
-            for link in finding_words:
-                words.append(link.find('a').get('href')[6:-1])     
-                
-            return words
-        
-        wordFind_dict[word_length] = extract(unextracted)
+            unextracted = BeautifulSoup(req.text, 'html.parser').find_all('li') 
+            wordFind_dict[word_length] = extract(unextracted)  
+        except requests.exceptions.ConnectionError as e:
+            print ("A connection error occurred.") 
         
     return wordFind_dict  
+
+
